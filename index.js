1db5376931af623a1be5bdcca3047efa80fb9ea9
LynWM <murathimilynette@gmail.com>
2026-04-15 13:14:13 +0300

    fix:Modified index.js file

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
+function calculateTax(amount) {
+    let tax = amount*0.1;
+
+    return tax;
+}
+// console.log(calculateTax(300));
 
 
+function convertToUpperCase (text) {
+    text = "money";
+    return text.toUpperCase();
+}
+
+// console.log(convertToUpperCase());
+
+function findMaximum (numI, numII) {
+    if (numI > numII) {
+        return maximum = numI;
+    } else if (numII > numI) {
+        return maximum = numII;
+    } else 
+        return "Numbers are equal";       
+}
+
+// console.log(findMaximum(9, 9));
+
+function isPalindrome(word) {
+
+    let reversed = word.split('').reverse().join('');
+
+    if (reversed === word) {
+        return word + " is a palindrome"
+    } else {
+        return reversed + " is not a palindrome"
+    }
+}
+
+// console.log(isPalindrome("madam"));
+
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let discount = originalPrice * discountPercentage;
+    let discountPrice = originalPrice - discount;
+
+    return discountPrice;
+}
+
+// console.log(calculateDiscountedPrice(1800, 0.4));
 
 
 
